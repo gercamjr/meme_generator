bcc1d994d07bc2ce4d94ac6794678844c08d6d87
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-21 17:17:39 -0600

    initialized header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,11 @@
-import React from 'react';
+import React from "react";
 import "../styles/styles.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Meme Generator</h1>
+      <h3>React Course - Project 3</h3>
+    </div>
+  );
+}
