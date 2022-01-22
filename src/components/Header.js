40bfa91d30070dfa3461950e53bd8580c5a93635
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-21 18:45:21 -0600

    finished styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ import "../styles/styles.css";
 export default function Header() {
   return (
     <div className="header">
-      <h1>Meme Generator</h1>
-      <h3>React Course - Project 3</h3>
+    <img src={`${process.env.PUBLIC_URL}/images/Troll-Face.png`} alt="troll face" className="header__logo" />
+      <h1 className="header__title">Meme Generator</h1>
+      <h3 className="header__subtitle">React Course - Project 3</h3>
     </div>
   );
 }
