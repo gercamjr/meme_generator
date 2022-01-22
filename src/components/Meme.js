2007b3a55b890593fdfbddb6a596db801f44ac63
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-21 19:44:07 -0600

    styling meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,9 +5,9 @@ export default function Meme() {
     return (
         <div className="meme">
             <form action="" className="meme__form">
-                <input type="text" placeholder="bottom text" className="meme__form--bottom"></input>
-                <input type="text" placeholder="upper text" className="meme__form--upper"></input>
-                <button className="meme__form--btn">Get a new meme image</button>
+                <input type="text" placeholder="bottom text" className="meme__form--bottom meme__form--inputs"></input>
+                <input type="text" placeholder="upper text" className="meme__form--upper meme__form--inputs"></input>
+                <button onClick className="meme__form--btn">Get a new meme image  🖼</button>
             </form>
         </div>
     )
