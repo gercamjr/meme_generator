22262af38315dadac7503690502ee0b93095aaaa
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-24 17:23:14 -0600

    adding logic to the meme generator button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Meme from './components/Meme';
-
+ 
 function App() {
   return (
     <div className="App">
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import "../styles/styles.css";
+import memeData from '../memesData.js';
+
 
 export default function Meme() {
+
     return (
         <div className="meme">
-            <form action="" className="meme__form">
+            <div className="meme__form">
                 <input type="text" placeholder="bottom text" className="meme__form--bottom meme__form--inputs"></input>
                 <input type="text" placeholder="upper text" className="meme__form--upper meme__form--inputs"></input>
-                <button onClick className="meme__form--btn">Get a new meme image  🖼</button>
-            </form>
+                <button onClick={grabMeme} className="meme__form--btn">Get a new meme image  🖼</button>
+            </div>
         </div>
     )
+
+    function grabMeme() {
+        
+        const prop = Math.floor(Math.random() * memeData.data.memes.length);
+        console.log("the meme url we picked is: ", memeData.data.memes[prop].url)
+        
+    }
 }
