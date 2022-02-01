a06f3745507e3e20feb700d16a38776478b91e81
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-02-01 17:39:11 -0600

    refactored to include use of react state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../styles/styles.css";
 import memeData from '../memesData.js';
 
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("");
+
+    function grabMeme() {
+        
+        const prop = Math.floor(Math.random() * memeData.data.memes.length);
+        const memeURL = memeData.data.memes[prop].url;
+        console.log("the meme url we picked is: ", memeURL);
+        setMemeImage(memeURL)
+        
+    }
 
     return (
         <div className="meme">
@@ -12,13 +23,8 @@ export default function Meme() {
                 <input type="text" placeholder="upper text" className="meme__form--upper meme__form--inputs"></input>
                 <button onClick={grabMeme} className="meme__form--btn">Get a new meme image  🖼</button>
             </div>
+            <img src={memeImage} alt="a meme image" className="meme__image" />
         </div>
     )
 
-    function grabMeme() {
-        
-        const prop = Math.floor(Math.random() * memeData.data.memes.length);
-        console.log("the meme url we picked is: ", memeData.data.memes[prop].url)
-        
-    }
 }
